54ed58c21a2734475f52d18b268b57725d9b0414
batacraft <lordhunter017@gmail.com>
2024-09-26 17:48:29 +0545

    password and passcode generator

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,7 +5,7 @@ import {  NavLink } from "react-router-dom";
 const HomePage = () => {
     return (
         <div className="w-full h-full">
-            <div className="w-screen h-screen flex justify-center items-center bg-black flex-wrap text-white gap-8 text-2xl">
+            <div className="w-screen h-screen flex justify-center items-center bg-black flex-wrap text-white gap-2 md:gap-4 lg:gap-8 text-2xl">
 
                 {/* Password Generator Section */}
                 <div className="bg-green-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-green-600 transition duration-300 ">
